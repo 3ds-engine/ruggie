ac2fdb48fff2c67bd150df44cd3c0076480e956c
DiegoCaballeroRuiz <dicaballero05@gmail.com>
2026-09-25 16:07:30 +0200

    Square moving

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,21 @@ fn main() {
     let apt = Apt::new().unwrap();
     let mut hid = Hid::new().unwrap();
     let gfx = Gfx::new().unwrap();
-    let _console = Console::new(gfx.top_screen.borrow_mut());
-
 
     ruggie_lib::init();
     let top_screen = ruggie_lib::create_top_screen();
 
+    let (mut x, mut y) = (0.0, 0.0);
     while apt.main_loop() {
         gfx.wait_for_vblank();
-
-        ruggie_lib::draw_square(top_screen);
         hid.scan_input();
         if hid.keys_down().contains(KeyPad::START) {
             break;
         }
+
+        ruggie_lib::draw_square(top_screen, x, y);
+        x += 1.0;
+        y += 1.0;
     }
     ruggie_lib::end();
 }
